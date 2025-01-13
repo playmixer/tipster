ae30546fc38a41de91f28f6d906b0a3ab1f75f92
shkirya.ns <shkirya.ns@oilpc.ru>
2025-01-13 21:16:18 +0600

    add btn collapsing controller

--- static/js/translator.js
+++ static/js/translator.js
@@ -9,6 +9,8 @@ const imgTranslate = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height=
 const imgUpload = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cloud-upload" viewBox="0 0 16 16">  <path fill-rule="evenodd" d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383"/>  <path fill-rule="evenodd" d="M7.646 4.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V14.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708z"/></svg>'
 const imgSpeech = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-volume-up" viewBox="0 0 16 16">  <path d="M11.536 14.01A8.47 8.47 0 0 0 14.026 8a8.47 8.47 0 0 0-2.49-6.01l-.708.707A7.48 7.48 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303z"/>  <path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.48 5.48 0 0 1 11.025 8a5.48 5.48 0 0 1-1.61 3.89z"/>  <path d="M10.025 8a4.5 4.5 0 0 1-1.318 3.182L8 10.475A3.5 3.5 0 0 0 9.025 8c0-.966-.392-1.841-1.025-2.475l.707-.707A4.5 4.5 0 0 1 10.025 8M7 4a.5.5 0 0 0-.812-.39L3.825 5.5H1.5A.5.5 0 0 0 1 6v4a.5.5 0 0 0 .5.5h2.325l2.363 1.89A.5.5 0 0 0 7 12zM4.312 6.39 6 5.04v5.92L4.312 9.61A.5.5 0 0 0 4 9.5H2v-3h2a.5.5 0 0 0 .312-.11"/></svg>'
 const imgRemove = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/></svg>'
+const imgChevronLeft = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">  <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/></svg>'
+const imgChevronRight = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">  <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/></svg>'
 
 let longTimeVoiceMessage = 5000 // 5 sec
 let languages = {
@@ -196,7 +198,7 @@ function speech(audio, btn, text, name, id, error, next) {
                 src = URL.createObjectURL(body)
                 audio.src = src
                 audio.load()
-                btn.classList.remove("hide")
+                // btn.classList.remove("hide")
                 if (next != null) {
                     next()
                 }
@@ -287,6 +289,7 @@ async function addAudio(src, name) {
 
     let playTranslated = document.createElement("button")
     playTranslated.innerHTML = imgPlay
+    playTranslated.title = "play translated"
     playTranslated.classList.value = "btn btn-primary hide"
 
     const eventPlaySpeech = audioPlay(audioTranslated, playTranslated)
@@ -299,17 +302,20 @@ async function addAudio(src, name) {
 
     let play = document.createElement("button")
     play.innerHTML = imgPlay
-    play.classList.value = "btn btn-primary"
+    play.title = "play original"
+    play.classList.value = "btn btn-primary hide"
     play.addEventListener("click", audioPlay(audio, play))
 
     let recognize = document.createElement("button")
     recognize.innerHTML = imgRecognize
-    recognize.classList.value = "btn btn-primary"
+    recognize.title = "recognize"
+    recognize.classList.value = "btn btn-primary hide"
     recognize.addEventListener("click", eventGetRecognize)
 
     let translate = document.createElement("button")
     translate.innerHTML = imgTranslate
-    translate.classList.value = "btn btn-primary"
+    translate.title = "translate"
+    translate.classList.value = "btn btn-primary hide"
     translate.addEventListener("click", eventGetTranslate)
 
 
@@ -320,13 +326,29 @@ async function addAudio(src, name) {
 
     let btnRemove = document.createElement("button")
     btnRemove.innerHTML = imgRemove
+    btnRemove.title = "remove"
     btnRemove.classList.value = "btn btn-primary"
     btnRemove.addEventListener("click", function() {
         message.remove()
     })
 
+    let btnHidding = document.createElement("button")
+    btnHidding.innerHTML = (name != "record2" ? imgChevronRight : imgChevronLeft)
+    btnHidding.classList.value = "btn btn-primary"
+    btnHidding.addEventListener("click", function() {
+        const hidy = hidding([play, recognize, translate, playTranslated], true)
+        return function() {
+            if (hidy()) {
+                btnHidding.innerHTML = (name != "record2" ? imgChevronRight : imgChevronLeft)
+            } else {
+                btnHidding.innerHTML = (name == "record2" ? imgChevronRight : imgChevronLeft)
+            }
+        }
+    }())
+
     let controler = document.createElement("div")
     controler.classList.add("msg_controler")
+    controler.appendChild(btnHidding)
     controler.appendChild(audio)
     controler.appendChild(play)
     controler.appendChild(sourceLang)
@@ -404,6 +426,21 @@ function renderLanguage() {
         li = document.createElement("li")
         li.appendChild(a2)
         lang2.append(li)
+    }
+}
+
+function hidding(elements, isHidden) {
+    return function() {
+        for (e of elements) {
+            console.log(e)
+            if (isHidden) {
+                e.classList.remove("hide")
+            } else {
+                e.classList.add("hide")
+            }
+        }
+        isHidden = !isHidden
+        return isHidden
     }
 }
 
